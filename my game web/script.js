501d594326a3bd372f38362eed8dfbe5fe02aee3
aryan-negi-93 <134124253+aryan-negi-93@users.noreply.github.com>
2024-03-02 23:10:41 +0530

    Add files via upload

---
+++ my game web/script.js
@@ -0,0 +1,11 @@
+var menulogo = document.querySelector('.menu')
+var header = document.querySelector('header')
+
+
+menulogo.addEventListener('click',function(){
+    // header.classlist.toggle('header')
+    // header.style.transform = "translateX(0)"
+    header.classList.toggle("show")
+    // header.style.border = "10px solid red"
+}
+)
